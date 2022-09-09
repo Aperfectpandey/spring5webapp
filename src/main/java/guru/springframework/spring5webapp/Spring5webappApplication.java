5f75c612c4b43ed79e9a0a116df7161967be42fc
Aperfectpandey <arvindpandeysbg@gmail.com>
2022-09-10 01:18:13 +0530

    1st commit done

--- src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
+++ src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
@@ -8,6 +8,6 @@ public class Spring5webappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Spring5webappApplication.class, args);
-		System.out.println("PerfectPandey-first commit done by pandey");
+		System.out.println("PerfectPandey -first commit done by pandey");
 	}
 }
