6abc58a30d379cb9dc1e4446e9f451a70e77be77
Aperfectpandey <arvindpandeysbg@gmail.com>
2022-09-12 18:24:03 +0530

    PerfectPan -first commit done by pandey branch 3

--- src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
+++ src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
@@ -8,6 +8,6 @@ public class Spring5webappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Spring5webappApplication.class, args);
-		System.out.println("PerfectPan -first commit done by pandey branch 2");
+		System.out.println("PerfectPan -first commit done by pandey branch 3");
 	}
 }
